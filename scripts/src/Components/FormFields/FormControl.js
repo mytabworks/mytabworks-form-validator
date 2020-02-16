9e333b5519d8bdec684f9de5b9a30e5a0ab53200
mytabworks <mytabworks@gmail.com>
2020-02-16 16:23:48 +0800

    fix prop children type

--- scripts/src/Components/FormFields/FormControl.js
+++ scripts/src/Components/FormFields/FormControl.js
@@ -7,15 +7,15 @@ const FormControlPropTypes = {
     rules: PropTypes.string,
     className: PropTypes.string,
     state: PropTypes.object,
-    children: PropTypes.element.isRequired, 
+    children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]).isRequired, 
 }
 
 const FormControlDefaultProps = { 
     className: ''
 }
 
-const FormControl = ({finalId, label, rules, className, children, state}) => (
-    <div className={`form-control ${className}`.trim()}>
+const FormControl = ({finalId, label, rules, className, children, state, ...props}) => (
+    <div className={`form-group ${className}`.trim()} {...props}>
         {label && <label htmlFor={finalId}>{label}{rules && /required(?!_)/.test(rules) && <span className="required">*</span>}</label>}
         {children}
         {state && state.isInvalid && <span className="error-msg">{state.message}</span>}
